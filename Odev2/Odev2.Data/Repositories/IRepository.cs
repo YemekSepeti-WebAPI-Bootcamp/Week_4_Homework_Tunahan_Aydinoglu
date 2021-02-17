eedb281a23b4ca3267dd573b341147040e315211
Tunahan <tunahanaydinoglu@outlook.com>
2021-02-17 23:02:53 +0300

    Homework2

---
+++ Odev2/Odev2.Data/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev2.Data.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+        TEntity Update(TEntity entity);
+    }
+}
